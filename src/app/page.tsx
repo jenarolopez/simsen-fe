c38360e28c22a663d31b5dae5bedbede8f26dfa3
jenarolopez <47820660+jenarolopez@users.noreply.github.com>
2025-04-26 17:35:47 +0800

    feat: add HeroSection component and integrate it into the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import HeroSection from "@/components/home/HeroSection";
 import Footer from "@/components/layout/footer";
-
 
 export default function Home() {
   return (
     <div className="w-full">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        
+        <HeroSection />
       </main>
       <Footer />
     </div>
---
+++ src/components/home/HeroSection.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+export default function HeroSection() {
+  return (
+    <section className="relative w-full flex items-center py-10">
+      <Image
+        aria-hidden
+        src="/images/hero.png"
+        alt="Travel Background"
+        fill={true}
+        objectFit="cover"
+        className="absolute"
+      />
+
+      <div className="max-w-[1800px] px-[5rem]">
+        <h1 className="text-3xl font-cursive text-white z-20 relative">
+          Simsem
+        </h1>
+
+        <div className="relative z-20 items-center justify-center h-full grid grid-cols-2">
+          <div className="text-white">
+            <h1 className="text-4xl md:text-5xl font-bold leading-tight mb-6">
+              Book your <span className="text-yellow-400 font-bold">FREE</span>{" "}
+              <br />
+              authentic experiences with <br /> verified locals!
+            </h1>
+            <p className="text-lg mb-6">
+              Are you a passionate{" "}
+              <span className="text-yellow-400">travel creator</span> looking to
+              explore the captivating culture, rich history, and stunning
+              landscapes of the Middle East?
+            </p>
+          </div>
+          <div className="flex items-center flex-row justify-center">
+            <div className="bg-white w-[420px] h-[689px] rounded-[15px] flex">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Logo */}
+    </section>
+  );
+}
