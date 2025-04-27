47523ccab72b1119468e86510cc1c5392ddc9d54
jenarolopez <47820660+jenarolopez@users.noreply.github.com>
2025-04-27 14:32:41 +0800

    fix: Update Firebase configuration to use environment variables for sensitive data

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDUe5KvAwomNgSHhwjfjAy4N1YCuLQ6cyg",
-  authDomain: "simsen-app.firebaseapp.com",
-  projectId: "simsen-app",
-  storageBucket: "simsen-app.firebasestorage.app",
-  messagingSenderId: "862796273058",
-  appId: "1:862796273058:web:a62219e911643cef2f0efb"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
