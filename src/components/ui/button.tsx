e44350e24265792d4a28bb8c7738500eb8e72524
jenarolopez <47820660+jenarolopez@users.noreply.github.com>
2025-04-29 00:29:46 +0800

    fix: Add cursor pointer style to button variants for better user experience

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cn } from "../../../lib/utils"
 
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:bg-gray-500 disabled:cursor-not-allowed rounded-none rounded-tl-4xl rounded-br-4xl",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:bg-gray-500 disabled:cursor-not-allowed rounded-none rounded-tl-4xl rounded-br-4xl cursor-pointer",
   {
     variants: {
       variant: {
